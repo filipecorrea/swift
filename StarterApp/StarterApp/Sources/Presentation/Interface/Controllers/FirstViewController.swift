028f5c5a40fbdc9e296c88b16aed95252421a628
Filipe Corrêa <Filipe@Filipes-IBM-MacBook-Pro.local>
2016-01-28 13:35:57 -0200

    New directory structure

---
+++ StarterApp/StarterApp/Sources/Presentation/Interface/Controllers/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StarterApp
+//
+//  Created by Filipe Corrêa on 06/01/16.
+//  Copyright © 2016 Filipe Corrêa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
